70403285386705659bdbdeeda3d63935f767b749
ismail benrahhal <ibenrahhal133@gmail.com>
2026-03-03 00:17:26 +0000

    get all students done.

--- PT/frmLogin.Designer.cs
+++ PT/frmLogin.Designer.cs
@@ -33,12 +33,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(304, 160);
+            this.button1.Location = new System.Drawing.Point(13, 14);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(642, 318);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Get all ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
